737808581f765f6f194d6f17fef47481be31d184
adagiomedia <adagiomediarus@gmail.com>
2025-04-06 09:47:32 +0200

    SNAP-1712 Tested on GiveWP 4.0.0

--- coinsnap-for-givewp.php
+++ coinsnap-for-givewp.php
@@ -12,7 +12,7 @@
  * Requires at least: 6.0
  * Tested up to:    6.7
  * Requires Plugins: give
- * GiveWP tested up to: 3.22.2
+ * GiveWP tested up to: 4.0.0
  * License:         GPL2
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  *
